f36621e8a1574184626e9a5e71ca3e3999432f4a
vineetatlani7@gmail.com <vineetatlani7@gmail.com>
2020-11-21 18:29:51 +0530

    added Login Support

--- src/main/java/com/davviips/ELearning/config/MySecurityConfig.java
+++ src/main/java/com/davviips/ELearning/config/MySecurityConfig.java
@@ -1,6 +1,10 @@
 package com.davviips.ELearning.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +13,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().permitAll();
 		
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
+	}
+
+	
+	
 	
 }
